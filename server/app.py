1d5a96da359337a2dbe8ccec7909c4c356b95631
Kelvin Muchiri <muchirikelv71@gmail.com>
2025-04-03 15:10:27 +0300

    solved tests

--- server/app.py
+++ server/app.py
@@ -1,8 +1,9 @@
 # server/app.py
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
+from sqlalchemy.orm import sessionmaker
 
 from models import db, Earthquake
 
@@ -14,14 +15,27 @@ app.json.compact = False
 migrate = Migrate(app, db)
 db.init_app(app)
 
+with app.app_context():
+    Session = sessionmaker(bind=db.engine)
+    session = Session()
 
 @app.route('/')
 def index():
     body = {'message': 'Flask SQLAlchemy Lab 1'}
     return make_response(body, 200)
 
-# Add views here
-
+@app.route('/earthquakes/<int:id>')
+def get_earthquake(id):
+    earthquake = session.get(Earthquake,id)
+    if earthquake:
+        return jsonify({
+            "id": earthquake.id,
+            "location": earthquake.location,
+            "magnitude": earthquake.magnitude,
+            "year": earthquake.year
+        }), 200
+    else:
+        return jsonify({"message": f"Earthquake {id} not found."}), 404
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
